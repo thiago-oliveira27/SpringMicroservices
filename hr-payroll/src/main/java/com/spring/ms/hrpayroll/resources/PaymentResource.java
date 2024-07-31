454103a112bc822a9142f8e42da51edf9020ac87
thiago <thiagoolcarv27@gmail.com>
2024-07-30 23:51:50 -0300

    hr-payroll implemented

--- hr-payroll/src/main/java/com/spring/ms/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/spring/ms/hrpayroll/resources/PaymentResource.java
@@ -1,9 +1,13 @@
 package com.spring.ms.hrpayroll.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.spring.ms.hrpayroll.entities.Payment;
 import com.spring.ms.hrpayroll.services.PaymentService;
 
 @RestController
@@ -14,11 +18,11 @@ public class PaymentResource {
 	private PaymentService service;
 	
 //	@HystrixCommand(fallbackMethod = "getPaymentAlternative")
-//	@GetMapping(value = "/{workerId}/days/{days}")
-//	public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
-//		Payment payment = service.getPayment(workerId, days);
-//		return ResponseEntity.ok(payment);
-//	}	
+	@GetMapping(value = "/{workerId}/days/{days}")
+	public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
+		Payment payment = service.getPayment(workerId, days);
+		return ResponseEntity.ok(payment);
+	}	
 //	
 //	public ResponseEntity<Payment> getPaymentAlternative(Long workerId, Integer days) {
 //		Payment payment = new Payment("Brann", 400.0, days);
--- hr-payroll/src/main/java/com/spring/ms/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/spring/ms/hrpayroll/services/PaymentService.java
@@ -2,15 +2,18 @@ package com.spring.ms.hrpayroll.services;
 
 import org.springframework.stereotype.Service;
 
+import com.spring.ms.hrpayroll.entities.Payment;
+import com.spring.ms.hrpayroll.entities.Worker;
+
 @Service
 public class PaymentService {
 	
 //	@Autowired
 //	private WorkerFeignClient workerFeignClient;
 //
-//	public Payment getPayment(long workerId, int days) {
-//				
-//		Worker worker = workerFeignClient.findById(workerId).getBody();
-//		return new Payment(worker.getName(), worker.getDailyIncome(), days);
-//	}
+	public Payment getPayment(long workerId, int days) {
+				
+		//Worker worker = workerFeignClient.findById(workerId).getBody();
+		return new Payment("Bob", 175.0, days);
+	}
 }
